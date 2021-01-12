b1d9186276fda1434d719cd97aeeb264d3ab7165
dexter5654 <72883601+dexter5654@users.noreply.github.com>
2021-01-12 13:14:46 -0300

    Add files via upload
    If e Else em javascript

---
+++ If&Else.js
@@ -0,0 +1,12 @@
+<script>
+var a=7;
+var h=7;
+var k=a+h;
+document.write(k);
+if(k>7){
+    document.write("esse numero é maior que 7");
+}
+if(k <7){
+    document.write("esse numero é menor que 7");
+}
+</script> 
