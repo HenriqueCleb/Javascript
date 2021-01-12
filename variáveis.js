a4dd900a3e7b5862a9062434cdb0ce67d19f90f2
dexter5654 <72883601+dexter5654@users.noreply.github.com>
2021-01-12 13:16:07 -0300

    Add files via upload
    Variáveis em js

---
+++ variáveis.js
@@ -0,0 +1,8 @@
+<script>
+var a =2;
+var b =5;
+var c= a +b;
+document.white(c);
+</script>
+// em js as variaveis recebem o "var " na frente dos seus respectivos nomes, sendo que o noma das variaveis deve conter caracteres de a...z(A...Z),conter "_"(underline) ou "$" em suas iniciais 
+//na linha 2 e 3  temos as declarações das variaveis e na linha 4 a soma das variaveis 
