b414f19b2e10488590369fc52fc5a1e03e69f0dc
dexter5654 <72883601+dexter5654@users.noreply.github.com>
2021-01-12 13:17:16 -0300

    Add files via upload
    Script básico de impressão na tela com js

---
+++ impressãonatela.js
@@ -0,0 +1,4 @@
+<script type = "text/js">
+	document.write("hello,world");
+	</script>
+	// o acima mostra uma uma mensagem na tela no caso a mensagem colocada entre parenteses na linha 2,na linha 3 a variavel é chamada 
